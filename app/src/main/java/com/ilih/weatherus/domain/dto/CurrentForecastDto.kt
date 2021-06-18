764294c464b1b88719ba19f478e92fa3f629698c
101b1 <101b1@rambler.ru>
2021-06-18 20:27:54 +0300

    current forecast dto

---
+++ app/src/main/java/com/ilih/weatherus/domain/dto/CurrentForecastDto.kt
@@ -0,0 +1,18 @@
+package com.ilih.weatherus.domain.dto
+
+data class CurrentForecastDto(
+        val temperature: Float,
+        val feelsLikeTemp: Float,
+        val pressure: Float,
+        val weaterIcon: String,
+        val weatherCode: Int,
+        val weatherDesc: String,
+        val windSpeed: Float,
+        val windDir: String,
+        val sunriseTime: String,
+        val sunsetTime: String,
+        val cloudCoverage: Float,
+        val uvIndex: Int,
+        val cityName: String,
+)
+
